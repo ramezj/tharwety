63f8a8b52da77f86a377084256d0ae24c8189e45
ramezj <ramezjoseph8@gmail.com>
2025-09-27 17:29:28 +0300

    tailwindcss & nativewind initiated on expo application

---
+++ application/app/_layout.tsx
@@ -0,0 +1,7 @@
+import { Stack } from "expo-router";
+import './globals.css'
+import React from "react";
+
+export default function RootLayout() {
+  return <Stack />;
+}
---
+++ application/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-primary font-extrabold">tharwety</Text>
+    </View>
+  );
+}
